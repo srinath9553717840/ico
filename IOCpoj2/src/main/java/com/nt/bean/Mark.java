b72c9076d976c7174faad8fdd267afeb653436d5
srinath9553717840 <madishettysrinath10@gmail.com>
2019-10-01 23:02:12 +0530

    change the file

---
+++ IOCpoj2/src/main/java/com/nt/bean/Mark.java
@@ -0,0 +1,22 @@
+package com.nt.bean;
+
+
+public class Mark{
+	
+	private int m1,m2,m3;
+
+	public Mark(int m1, int m2, int m3) {
+		
+		this.m1 = m1;
+		this.m2 = m2;
+		this.m3 = m3;
+	}
+
+	@Override
+	public String toString() {
+		return "Mark [m1=" + m1 + ", m2=" + m2 + ", m3=" + m3 + "]";
+	}
+						
+	
+}
+
---
+++ IOCpoj2/src/main/java/com/nt/bean/Test.java
@@ -0,0 +1,20 @@
+package com.nt.bean;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class Test {
+public static void main(String[] args) {
+	
+     FileSystemResource resource=new FileSystemResource("src/main/java/com/nt/bean/application.xml");
+     
+     XmlBeanFactory factory=new  XmlBeanFactory(resource);
+     
+     Mark bean=(Mark)factory.getBean("mk",Mark.class);
+     
+     String result=bean.toString();
+     
+     System.out.println(result);
+	}
+
+}
